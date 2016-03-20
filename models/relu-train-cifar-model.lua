6d358422b86776d0ca2ef2cb238617d2cb0d3931
Yutaro Yamada <yutaro.yamada0424@gmail.com>
2016-03-20 05:00:24 -0400

    add relu model

---
+++ models/relu-train-cifar-model.lua
@@ -0,0 +1,21 @@
+require 'nn'
+
+local model = nn.Sequential()
+------------------------------------------------------------
+-- convolutional network
+------------------------------------------------------------
+-- stage 1 : mean+std normalization -> filter bank -> squashing -> max pooling
+model:add(nn.SpatialConvolutionMap(nn.tables.random(3,16,1), 5, 5))
+model:add(nn.ReLU())
+model:add(nn.SpatialMaxPooling(2, 2, 2, 2))
+-- stage 2 : filter bank -> squashing -> max pooling
+model:add(nn.SpatialConvolutionMap(nn.tables.random(16, 256, 4), 5, 5))
+model:add(nn.ReLU())
+model:add(nn.SpatialMaxPooling(2, 2, 2, 2))
+-- stage 3 : standard 2-layer neural network
+model:add(nn.Reshape(256*5*5))
+model:add(nn.Linear(256*5*5, 128))
+model:add(nn.ReLU())
+model:add(nn.Linear(128,#classes))
+
+return model
